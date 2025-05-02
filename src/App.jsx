c4b64b813278998998014dbca1524348c086abcd
raghavbhatia1 <raghavbhatia1276@gmail.com>
2025-05-02 16:56:42 +0530

    first work on ci cd pipeline

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>my react app</h1>
+     <h2>my react app implement</h2>
+     <h1>with github actions</h1>
+    </>
+  )
+}
+
+export default App
